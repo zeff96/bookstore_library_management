61623f5caf3c8aab0f755adc5b5dc21f82d3339d
zeff96 <zeffo1975@gmail.com>
2024-07-21 21:58:00 +0300

    Add routes for non_fictions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +7,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "root#index"
+  resources :non_fictions, only: %i[index]
 end
