97b8802f2b6d173471fad37918106bcc7b032a6a
zeff96 <zeffo1975@gmail.com>
2024-08-26 20:25:13 +0300

    Update routes to include blog routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :academic_professionals, only: %i[index]
   resources :business_managements, only: [:index]
   resources :history_politics, only: %i[index]
+  resources :blogs, only: %i[index]
 end
