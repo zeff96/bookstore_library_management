c55161241a6b319f27917b23733092726cb4d5c6
zeff96 <zeffo1975@gmail.com>
2024-10-02 21:30:57 +0300

    Update books schema migration file

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_03_202040) do
   create_table "books", force: :cascade do |t|
     t.string "image"
     t.string "title"
-    t.string "author"
+    t.decimal "price", precision: 10, scale: 2
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
